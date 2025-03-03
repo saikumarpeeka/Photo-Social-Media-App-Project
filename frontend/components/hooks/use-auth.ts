1e038ecb5453011ca44751d3a501641cdd6dba1c
SAIKUMAR PEEKA <saikumarr.peeka@gmail.com>
2025-03-03 20:37:07 +0530

    use-auth.ts

--- frontend/components/hooks/use-auth.ts
+++ frontend/components/hooks/use-auth.ts
@@ -4,8 +4,6 @@ import { useDispatch } from "react-redux"
 import { handleAuthRequest } from "../utils/apiRequest";
 import { setAuthUser } from "@/store/authSlice";
 import { toast } from "sonner";
-import { useState } from "react";
-
 
 export const useFollowUnfollow = () => {
     const dispatch = useDispatch();
