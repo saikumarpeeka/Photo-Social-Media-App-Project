992b78188ebf8b9626805a5fc87bd54190a3e228
SAIKUMAR PEEKA <saikumarr.peeka@gmail.com>
2025-03-03 23:13:21 +0530

    authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -28,7 +28,7 @@ const createSendToken = (user, statusCode, res, message)=> {
             Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_ENV === "production" ? true : false,
         sameSite: process.env.NODE_ENV === "production" ? "none" : "Lax",
     };
 
@@ -196,7 +196,7 @@ exports.logout = catchAsync(async(req,res,next)=>{
     res.cookie("token", "loggedout", {
         expires: new Date(Date.now() + 10*1000),
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_ENV === "production" ? true : false,
     });
 
     res.status(200).json({
